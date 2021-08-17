25b6d6de8b960bcd4e663e598a2f47bc2d0072b1
Yufei <fay.z@163.com>
2021-08-16 22:07:06 -0700

    build index page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import time
 import os
 import librosa
 import numpy as np
-from flask import Flask, jsonify, request 
+from flask import Flask, jsonify, request, render_template, redirect, url_for
 import model.clip2frame
 from model.test_model_preppipline import predict_curSong
 from model.test_model_preppipline import feat_extract
@@ -13,22 +13,21 @@ from model.cnn import score_pred_only
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
-def infer_mp3():
-    # Catch the image file from a POST request
-    if 'file' not in request.files:
-        return "Please try again. The mp3 file doesn't exist"
-    
-    # get file
-    file = request.files.get('file')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/', methods=['POST','GET'])
+def upload_file():
+    uploaded_file = request.files['file']
     # temporarily save file 
     store_dir = os.path.join(os.path.dirname(__file__), 'filestorage')
     if not os.path.isdir(store_dir):
         os.mkdir(store_dir)
-    file_name = os.path.join(store_dir, 'test.mp3')
-    file.save(file_name)
-
+    if uploaded_file.filename != '':
+        file_name = os.path.join(store_dir, 'test.mp3')
+        uploaded_file.save(file_name)
+    
     # predict
     mel_Feature = feat_extract(file_name)
     tag_feature = predict_curSong(file_name)
@@ -37,12 +36,7 @@ def infer_mp3():
     # remove file
     os.remove(file_name)
 
-    # Return on a JSON format
     return jsonify(score)
-
-@app.route('/', methods=['GET'])
-def index():
-    return 'Machine Learning Inference'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
